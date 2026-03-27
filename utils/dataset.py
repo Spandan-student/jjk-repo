f93ef270283a4c73b065e80c0e9ae06ffaee2f2c
SpandyX007 <spandanar1234@gmail.com>
2026-03-28 01:16:01 +0530

    create dataset file to train hand signs

--- utils/dataset.py
+++ utils/dataset.py
@@ -8,7 +8,7 @@ mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 hands = mp_hands.Hands(
     static_image_mode=False,
-    max_num_hands=1,
+    max_num_hands=2,
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5
 )
